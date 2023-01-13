da70a8e1a13baf998d673f1d057dd56fddb583a2
Tola Awofolu <oawofolu@pivotal.io>
2023-01-13 00:42:48 -0500

    updated Readme

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='scdfmlutils',
+    name='scdfrabbitutils',
     version='2.0',
     packages=find_packages(),
     install_requires=[
