571acf3ae7feadda68b02c84a7127b7f68b2912a
Tola Awofolu <oawofolu@pivotal.io>
2023-01-13 00:32:48 -0500

    updated dir structure

--- setup.py
+++ setup.py
@@ -3,5 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name='scdfmlutils',
     version='2.0',
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        'pika',
+        'streamlit==1.12.0'
+    ]
 )
